3df0bb4089016acb2cc82d77391fa709f736bb91
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-22 19:49:58 +0900

    add url-loader

--- editor/next.config.js
+++ editor/next.config.js
@@ -6,6 +6,16 @@ const withTM = require("next-transpile-modules")([
 module.exports = withTM({
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+        },
+      },
+    });
+
+    config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
