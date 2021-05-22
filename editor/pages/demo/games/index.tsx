feb2698a6e08ad198b6beae74a44a25921ba4b43
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-23 03:37:07 +0900

    format view

--- editor/pages/demo/games/index.tsx
+++ editor/pages/demo/games/index.tsx
@@ -1,17 +1,24 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 
 export default function GamesDemoIndexPage() {
   return (
     <>
-      <li>
-        <Link href="./games/games-tanks-demo">Tanks demo</Link>
-      </li>
-      <li>
-        <Link href="./games/games-dragon-crushers">Dragon Crushers demo</Link>
-      </li>
-      <li>
-        <Link href="./games/games-endless-runner">Endless Runner demo</Link>
-      </li>
+      <Wrap>
+        <li>
+          <Link href="./games/games-tanks-demo">Tanks demo</Link>
+        </li>
+        <li>
+          <Link href="./games/games-dragon-crushers">Dragon Crushers demo</Link>
+        </li>
+        <li>
+          <Link href="./games/games-endless-runner">Endless Runner demo</Link>
+        </li>
+      </Wrap>
     </>
   );
 }
+
+const Wrap = styled.div`
+  padding: 24px;
+`;
