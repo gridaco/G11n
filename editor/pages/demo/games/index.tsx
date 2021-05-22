949d375f7e38db36bd040b7f9f5559905069feb9
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-23 03:36:11 +0900

    ad li

--- editor/pages/demo/games/index.tsx
+++ editor/pages/demo/games/index.tsx
@@ -3,9 +3,15 @@ import Link from "next/link";
 export default function GamesDemoIndexPage() {
   return (
     <>
-      <Link href="./games/games-tanks-demo">Tanks demo</Link>
-      <Link href="./games/games-dragon-crushers">Dragon Crushers demo</Link>
-      <Link href="./games/games-endless-runner">Endless Runner demo</Link>
+      <li>
+        <Link href="./games/games-tanks-demo">Tanks demo</Link>
+      </li>
+      <li>
+        <Link href="./games/games-dragon-crushers">Dragon Crushers demo</Link>
+      </li>
+      <li>
+        <Link href="./games/games-endless-runner">Endless Runner demo</Link>
+      </li>
     </>
   );
 }
