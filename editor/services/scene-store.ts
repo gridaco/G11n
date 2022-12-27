c0d5ec776fa48a65a305322572d37046262d3f9a
softmarshmallow <woojoo@softmarshmallow.com>
2022-12-27 20:29:39 +0900

    add ngrok proxy for auth on dev

--- editor/services/scene-store.ts
+++ editor/services/scene-store.ts
@@ -1,18 +1,23 @@
 import { SceneStoreService } from "@base-sdk/scene-store";
 
 export function makeService() {
-  if (process.env.NODE_ENV == "development") {
-    // ONLY USED FOR DEVELOPMENT
-    // ssr
-    if (typeof window !== "undefined") {
+  if (typeof window !== "undefined") {
+    if (process.env.NODE_ENV == "development") {
+      // if location == webdev.grida.co
+      if (window.location.hostname == "webdev.grida.co") {
+        return new SceneStoreService({
+          type: "auto-browser-otp",
+        });
+      }
+      // ONLY USED FOR DEVELOPMENT
       return new SceneStoreService({
         type: "token",
         token: window.localStorage.getItem("contributors.grida.co/token"),
       });
+    } else {
+      return new SceneStoreService({
+        type: "auto-browser-otp",
+      });
     }
-  } else {
-    return new SceneStoreService({
-      type: "auto-browser-otp",
-    });
   }
 }
