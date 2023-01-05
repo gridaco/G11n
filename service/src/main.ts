226fddccd184542df60ead7bc98da8c3d688348c
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-05 20:09:11 +0900

    update service port to 3307

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,19 +1,19 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Text Set')
-    .setDescription('text set api description')
-    .setVersion('1.0')
+    .setTitle("Text Set")
+    .setDescription("text set api description")
+    .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(3307);
 }
 bootstrap();
