4711db27c854d71d86625e5de958a0c8f346603f
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-05 20:35:48 +0900

    add cors

--- service/src/main.ts
+++ service/src/main.ts
@@ -5,6 +5,8 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle("Text Set")
     .setDescription("text set api description")
