f5c4dc48c36bcc23117febf3df8f5a13ab3c2290
ywnam123 <ywnam@hatiolab.com>
2023-01-12 23:03:29 +0900

    fix many

--- service/src/main.ts
+++ service/src/main.ts
@@ -8,12 +8,12 @@ async function bootstrap() {
   app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle("Text Set")
-    .setDescription("text set api description")
+    .setTitle("Globalization")
+    .setDescription("globalization api description")
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("/", app, document);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3307);
