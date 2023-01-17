57fe610788e0b3af38b0ade4a202908bb166a909
Universe <woojoo@softmarshmallow.com>
2023-01-18 08:24:54 +0900

    init blank pages

---
+++ editor/pages/demo/v1/projects/[id]/export.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+import React from "react";
+export default function () {
+  const router = useRouter();
+  const { id } = router.query
+  return <div>{id} Export</div>;
+}
