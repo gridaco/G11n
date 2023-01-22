4371cf899a699a9724da077b2de22d51a47caa63
ywnam123 <ywnam@hatiolab.com>
2023-01-22 18:07:17 +0900

    add editor-ui submodule

--- editor/next.config.js
+++ editor/next.config.js
@@ -1,8 +1,11 @@
-const withTM = require("next-transpile-modules")([]);
+const withLinaria = require("next-linaria");
+const withTM = require("next-transpile-modules")(["@editor-ui/console"]);
 
-module.exports = withTM({
-  /**
-   * https://console.grida.co/globalization
-   */
-  basePath: "/globalization",
-});
+module.exports = withTM(
+  withLinaria({
+    /**
+     * https://console.grida.co/globalization
+     */
+    basePath: "/globalization",
+  })
+);
