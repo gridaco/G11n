61d9aa8cbc6cffcf533fe270483c223e6ad85cb6
ywnam123 <ywnam@hatiolab.com>
2023-02-14 21:26:10 +0900

    fix back buttons

--- editor/pages/projects/new/3/[id].tsx
+++ editor/pages/projects/new/3/[id].tsx
@@ -3,6 +3,7 @@ import Axios from "axios";
 import { useRouter } from "next/router";
 import { Button, TextFormField } from "@editor-ui/console";
 import styled from "@emotion/styled";
+import BackButton from "components/button/back";
 
 export default function setStartPages() {
   const router = useRouter();
@@ -29,27 +30,7 @@ export default function setStartPages() {
   return (
     <Page>
       <Toolbar style={{ width: "100%" }}>
-        <svg
-          width="40"
-          height="40"
-          viewBox="0 0 15 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          onClick={() => router.push(`/projects/new/2/${id}`)}
-          style={{
-            cursor: "pointer",
-            backgroundColor: "white",
-            border: "1px solid black",
-            borderRadius: 3,
-          }}
-        >
-          <path
-            d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
-            fill="currentColor"
-            fillRule="evenodd"
-            clipRule="evenodd"
-          ></path>
-        </svg>{" "}
+        <BackButton url={`/projects/new/2/${id}`}></BackButton>
       </Toolbar>
       <Body>
         <FormPart>
--- editor/scaffolds/editor/inner-editor-workspace.tsx
+++ editor/scaffolds/editor/inner-editor-workspace.tsx
@@ -4,6 +4,7 @@ import { Resizable } from "re-resizable";
 import styled from "@emotion/styled";
 import { editorState } from "states/text-editor.state";
 import CanvasPreview from "../preview";
+import BackButton from "components/button/back";
 
 export function InnerEditorWorkspace(props: {
   canvas: JSX.Element;
@@ -12,33 +13,50 @@ export function InnerEditorWorkspace(props: {
   const [, setIsSelect] = useRecoilState(editorState);
 
   return (
-    <Wrapper>
-      <CanvasPreview
-        onBackgroundClick={(e) => {
-          console.log(e);
-          setIsSelect(false);
-        }}
-      >
-        {props.canvas}
-      </CanvasPreview>
-      <Resizable
-        style={{
-          paddingBottom: 0,
-          backgroundColor: "#fff",
-          display: "flex",
-          flexDirection: "column",
-        }}
-        defaultSize={{
-          width: "50%",
-          height: "calc(100vh - 56px)",
-        }}
-        maxWidth="50%"
-        minWidth="20%"
-        maxHeight="calc(100vh - 56px)"
-      >
-        {props.editor}
-      </Resizable>
-    </Wrapper>
+    <>
+      <TopBar></TopBar>
+      <Wrapper>
+        <CanvasPreview
+          onBackgroundClick={(e) => {
+            console.log(e);
+            setIsSelect(false);
+          }}
+        >
+          {props.canvas}
+        </CanvasPreview>
+        <Resizable
+          style={{
+            paddingBottom: 0,
+            backgroundColor: "#fff",
+            display: "flex",
+            flexDirection: "column",
+          }}
+          defaultSize={{
+            width: "50%",
+            height: "calc(100vh - 56px)",
+          }}
+          maxWidth="50%"
+          minWidth="20%"
+          maxHeight="calc(100vh - 56px)"
+        >
+          {props.editor}
+        </Resizable>
+      </Wrapper>
+    </>
+  );
+}
+
+function TopBar() {
+  return (
+    <div
+      style={{
+        height: 56,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <BackButton url="/projects" />
+    </div>
   );
 }
 
@@ -46,5 +64,4 @@ const Wrapper = styled.div`
   display: flex;
   overflow: hidden;
   justify-content: space-between;
-  margin-top: 56px;
 `;
